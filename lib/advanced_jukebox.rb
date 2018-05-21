dc2fc309bf83ac429330fa298b3e0248af9501df
iwilliams83 <indirajamurath@gmail.com>
2018-05-21 20:40:09 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -13,33 +13,60 @@
 # }
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
-
-
-def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
-  #list the songs by name
+def list(song_list)
+  puts "Here is a list of songs you can play:"
+  song_list.each_with_index do |s,i|
+    puts "#{i+1}. #{s}"
+  end
 end
 
+def play(song_list)
+  count = song_list.length
+  puts "Please enter a song name or number:"
+  song_choice = gets.chomp
+  if song_list.include?(song_choice)
+    puts "Playing #{song_choice}"
+  elsif (1..count).include?(song_choice.to_i)
+    i = song_choice.to_i
+    i = i-1
+    puts "Playing #{song_list[i]}"
+  else
+    puts "Invalid input, please try again"
+  end
 
-def play(my_songs)
-  #this method is slightly different!
-  #you should still ask the user for input and collect their song choice
-  #this time, only allow user's to input a song name
-  #check to see if the name they give is in fact a key of the my_songs hash
-  #if it isn't, tell them their choice is invalid
-  #if it is, play the song using the system 'open <file path>' syntax
-  #get the file path of the song by looking it up in the my_songs hash
-  
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
-def run(my_songs)
-  #this method is the same as in jukebox.rb
+def run(songs)
+
+  puts "Please enter a command:"
+  command = gets.chomp
+
+  while command != "exit"
+    if command == "play"
+      play(songs)
+    elsif command == "list"
+      list(songs)
+    elsif command == "help"
+      help
+    else
+      puts "Invalid Command"
+    end
+    puts "Please enter a command"
+    command = gets.chomp
+  end
+
+  if command == "exit"
+    exit_jukebox
+  end
 end
